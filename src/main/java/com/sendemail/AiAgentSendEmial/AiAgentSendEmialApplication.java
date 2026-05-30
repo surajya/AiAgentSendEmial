4a77c8cc712740a44439a42702476b47358ba4ed
surajvns1571999 <surajvns1571999@gmail.com>
2026-05-30 14:31:01 +0530

    first commit

---
+++ src/main/java/com/sendemail/AiAgentSendEmial/AiAgentSendEmialApplication.java
@@ -0,0 +1,13 @@
+package com.sendemail.AiAgentSendEmial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAgentSendEmialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAgentSendEmialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sendemail/AiAgentSendEmial/AiAgentSendEmialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sendemail.AiAgentSendEmial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAgentSendEmialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
